ec91d5b54aa339a0ec7d33159a0903f10d62c44e
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2017-09-29 22:36:55 +0200

    EULA and certificate

--- app/src/main/java/dhbw/timetable/navfragments/preferences/PreferencesActivity.java
+++ app/src/main/java/dhbw/timetable/navfragments/preferences/PreferencesActivity.java
@@ -96,7 +96,7 @@ public class PreferencesActivity extends AppCompatPreferenceActivity {
             infoButton.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                 @Override
                 public boolean onPreferenceClick(Preference preference) {
-                    InfoDialog.newInstance("About DHBW Timetable", "This app is a project from students of the DHBW Stuttgart.\n\nIt's deployed with\n\nNO WARRANTY\n\nfor correctness or availability.\n\nHendrik Ulbrich, Malte Bartels (C) 2017").show(getFragmentManager(), "info");
+                    InfoDialog.newInstance("About DHBW Timetable", "This app is a project from students of the DHBW Stuttgart.\n\nIt's deployed with\n\nNO WARRANTY\n\nfor correctness or availability.\n\nHendrik Ulbrich, Malte Bartels (c) 2017\n\nhttp://ec.europa.eu/justice/data-protection/article-29/documentation/opinion-recommendation/files/2013/wp202_en.pdf").show(getFragmentManager(), "info");
                     return true;
                 }
             });
