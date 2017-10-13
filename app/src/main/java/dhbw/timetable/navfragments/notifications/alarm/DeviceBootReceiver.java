fd03e9f5d2380225fa3ab05db663be34725f24b3
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2017-10-13 11:44:28 +0200

    Fixed reboot and layout.

--- app/src/main/java/dhbw/timetable/navfragments/notifications/alarm/DeviceBootReceiver.java
+++ app/src/main/java/dhbw/timetable/navfragments/notifications/alarm/DeviceBootReceiver.java
@@ -11,12 +11,13 @@ import dhbw.timetable.data.TimetableManager;
 public class DeviceBootReceiver extends BroadcastReceiver {
 
     @Override
-    public void onReceive(final Context context, Intent intent) {
+    public void onReceive(final Context context, final Intent intent) {
         if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
             Log.i("BOOT", "Boot completed. Loading offline globals and resetup alarms");
             TimetableManager.getInstance().loadOfflineGlobals((Application) context.getApplicationContext(), new Runnable() {
                 @Override
                 public void run() {
+                    AlarmSupervisor.getInstance().initialize(context.getApplicationContext());
                     AlarmSupervisor.getInstance().rescheduleAllAlarms(context.getApplicationContext());
                 }
             });
