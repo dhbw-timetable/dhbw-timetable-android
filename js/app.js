ea026f6f3f87c01f62e2a6c8295f8dc64d434084
MalteBartels <malte.bartels@live.de>
2017-10-14 09:37:37 +0200

    Just redirection, nothing else. Fixed typo

--- js/app.js
+++
@@ -1,9 +0,0 @@
-const scrollButton = document.querySelector('.js-scroll-button');
-const letsGo = document.getElementById('letsgo');
-const body = document.getElementsByTagName('body')[0];
-
-scrollButton.addEventListener('click', e => {
-  $('html, body').animate({
-    scrollTop: $("#letsgo").position().top
-}, 750);
-});
